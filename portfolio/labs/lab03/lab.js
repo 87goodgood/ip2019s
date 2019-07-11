36407518de53d2e4982d3384518ee1974cd96ca2
87goodgood <52560218+87goodgood@users.noreply.github.com>
2019-07-11 18:12:03 +0800

    Add files via upload

---
+++ portfolio/labs/lab03/lab.js
@@ -0,0 +1,72 @@
+/*
+	https://www.html5rocks.com/en/tutorials/file/dndfiles/
+*/
+
+function readFiles(files) {
+    //files is a Fileist of File objects. List some properties.
+    var output=[];
+    for (var i =0,f; f = files[i]; i++) {
+        output.push('<li><strong>',escape(f.name), '</strong>'(',f.type || 'n/a', ') - ',
+            f.size, 'bytes, last modified; ',
+            f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString():'n/a',
+            '</li>');
+ }
+ document.getElementById('list').innerHTML = '<ul>'+output.join('')+'</ul>';
+
+
+ document.getElementById('content').innerHTML = '';
+ document.getElementById('svgimage').innerHTML = ='';
+
+ //Loop through the FileList and render image files as thumbnails
+ for (var i = 0,f; f = files[i];i++){
+    var reader = new FileReader();
+    reader.readAsText(files[i], 'UTF-8');
+
+    reader.onload =  function(evt) {
+        var span = document.createElement('span',null);
+        span.setAttribute("class", "svgshow");
+        span.innerHTML = evt.target.result;
+        document.getElementById('svgimage').insertBefore(span,null);
+
+        var span = document.createElement('span');
+        span.setAttribute("class","svgtext");
+        span.textContent = evt.target.result;
+        document.getElementById('content').insertBefore(span, null);
+
+        }
+     }
+}
+funtion  handleFileSelect(evt){
+    var files = evt.target.files;//FileList object
+
+    readFiles(files);
+
+}
+
+function handleFileSelect_drag(evt){
+    evt.stopPropagation();
+    evt.preventDefault;
+    var files = evt.dataTransfer.files;// FileList object.
+
+    readFiles(files);
+
+}
+
+function handleDragOver (evt) {
+ evt.stopPropagation();
+ evt.preventDefault;
+ evt.datdaTransfer.dropEffect = 'copy';// Explicity show this is a copy.
+
+}
+
+function start(e){
+    document.getElementById('files').addEventListener('change', handleFileSelect, faise);
+
+    var dropZone = document.getElementById('drop_zone');
+    dropZone.addEventListener('dragover',handleDragOver,false);
+    dropZone.addEventListener('drop',handleFileSelect_drag,false);
+} 
+
+
+ 
+window.addEventListener( "load", start, false );
