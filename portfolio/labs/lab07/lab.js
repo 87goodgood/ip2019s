b1742442b44576476bd16d238f6bc9e75de45267
87goodgood <52560218+87goodgood@users.noreply.github.com>
2019-07-12 18:01:52 +0800

    Add files via upload

--- portfolio/labs/lab07/lab.js
+++ portfolio/labs/lab07/lab.js
@@ -23,16 +23,66 @@ function analyze() {
         return;
       }
 
-      m = remain.search(/m/i);
-      console.log('m = ' + m);
+      m = remain.search('d');
+      console.log('d= m='+m);
+      remain = remain.slice(m);
 
-      n = remain.search('z"'); // /z/i
-      console.log('n = ' + n);
+
+      m= remain.search(/m/i);
+      console.log('m  m = '+ m);
+
+      n=remain.search(/z/i);'z"'  /z/i
+      console.log('z  n = ' +n );
+
 
       subs = remain.slice(m, n+1); // z 也要包含
-      conssole.log('subs = '+)
       console.log('subs = ' + subs);
 
+      var subs2 = subs.replace('440','480');
+      var subs2 = subs2.replace('462','262');
+
+      var path = image.path(subs).fill('black').stroke({color:'gray',width:5}).draggable();
+
+      path.plot(subs2).draggable();
+      //path.animate(2000).plot(subs2).loop(true,true).draggable();
+
+      var newPath = Snap.path.toCubic(subs);
+      console.log(' newPath.length = ' + newPath.length);
+      //console.table(' newPath = ' + newPath);
+
+      newPath.forEah(function(element){
+        console.log(element);
+      });
+      
+      for (var i = 0;i<(newPath.length - 1);i++){
+        for (var j = 0;j < newPath[i].lenrth; j++){
+          console.log( 'newPath[' + i + '][j] ='+newPath[i][j] );
+        }
+      
+      //if (newPath[i][1] == 'M' || newPath[i][1] == 'm' ){ // M i == 0
+        if( i == 0){ //M i == 0
+          var circle = image.circle(20).fill('red').stroke('blue').move(newPath[i][1]-10,newPath[i][2]-10.draggable();
+      }else { // C
+        var circle = image.circle(20).fill('pink').stroke('blue').move(newPath[i][1]-5,newPath[i][2]-5.draggable();
+          var circle = image.circle(20).fill('pink').stroke('blue').move(newPath[i][3]-5,newPath[i][4]-5.draggable();
+            var circle = image.circle(20).fill('pink').stroke('blue').move(newPath[i][5]-5,newPath[i][6]-5.draggable();
+     }
+   }
+
+    /*
+      //for(var i = 0; i < newPath.length; i++){
+      //for(var i = 0; i < (newPath.length - 1); i++){  // 抵銷因為 z 而多出了的最後三個點
+      for(var i = 0; i < (newPath.length - 1); i++){  // 抵銷因為 z 而多出了的最後三個點
+        var segment = newPath[i], point;
+
+        segment.shift();
+        point = setUpPoint(segment);
+      }
+
+      */
+   //for(var i = 0;i< newPath.length;i++){
+   llfor(var i =0;i<(newPath.length - 1);i++){ // 
+   }
       remain = remain.slice(n+1); // z 也要移除
       //console.log('remain = ' + remain);
 
@@ -41,7 +91,7 @@ function analyze() {
 
       var newPath = [];
       var move = '';
-
+pink
       m = subs.search(/c/i);
       console.log('m = subs.search(/c/i);');
       console.log('m = ' + m);
@@ -98,17 +148,7 @@ function analyze() {
 
       console.log(' newPath.length = ' + newPath.length);
 
-      /*
-      //for(var i = 0; i < newPath.length; i++){
-      //for(var i = 0; i < (newPath.length - 1); i++){  // 抵銷因為 z 而多出了的最後三個點
-      for(var i = 0; i < (newPath.length - 1); i++){  // 抵銷因為 z 而多出了的最後三個點
-        var segment = newPath[i], point;
-
-        segment.shift();
-        point = setUpPoint(segment);
-      }
-
-      */
+     
     } while (m > 0);
 }
 
